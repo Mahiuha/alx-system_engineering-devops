4acac995993d4478bf3f3894dd8b71f2c2018892
Mahiuha <tiasho.mahiuha@gmail.com>
2022-01-04 01:15:16 +0300

    create's a file in /tmp.

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#This creates a file in the /tmp directory.
+
+file { 'create':
+    path => '/tmp/school',
+    mode => '0744',
+    owner => 'www-data',
+    group => 'www-data',
+    content => 'I love Puppet',
+}
