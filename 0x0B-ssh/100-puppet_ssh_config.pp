f4d0ae583b4b3f71db3a7e1c81cb041d84a92bd7
Mahiuha <tiasho.mahiuha@gmail.com>
2022-01-05 02:12:14 +0300

    code refactor

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,9 @@
+# Client configuration file with Puppet
+file_line {  'passw_no_autentication':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
+}
+file_line {  'passw_direction':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/holberton',
+}
